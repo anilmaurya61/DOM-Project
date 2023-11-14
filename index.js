79ff6bc1daeda30435a62c4e3b0f7945b798a8dc
anilmaurya61 <anilkumarmaurya246@gmail.com>
2023-11-14 19:01:43 +0530

    Implemented CRUD Routes

--- index.js
+++ index.js
@@ -1,27 +1,89 @@
-const accessToken = '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'; 
-const username = 'anilmaurya61'; 
+const { Octokit } = require('@octokit/core');
+require('dotenv').config()
 
-const apiUrl = `https://api.github.com/users/${username}`;
+const { ACCESS_TOKEN, OWNER, REPO } = process.env;
 
-// Set up the headers with the access token
-const headers = {
-  Authorization: `Bearer ${accessToken}`,
-  Accept: 'application/json',
-};
 
-// Make the request using fetch
-fetch(apiUrl, { headers })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`Failed to fetch user. Status code: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(user => {
-    console.log(JSON.stringify(user,null,2));
-    console.log(`User found: ${user}`);
-    // Process user data as needed
-  })
-  .catch(error => {
-    console.error(error.message);
+async function getIssues() {
+  const octokit = new Octokit({
+    auth: ACCESS_TOKEN
   });
+
+  try {
+    const response = await octokit.request('GET /repos/anilmaurya61/DOM-project/issues', {
+      owner: OWNER,
+      repo: REPO,
+    });
+    console.log('Issue get successfully:', response.data);
+  } catch (error) {
+    console.error('Error creating issue:', error.message);
+  }
+}
+
+
+async function createdIssue() {
+  const octokit = new Octokit({
+    auth: ACCESS_TOKEN
+  })
+  try {
+    await octokit.request('POST /repos/{owner}/{repo}/issues', {
+      owner: OWNER,
+      repo: REPO,
+      title: 'Found a bug 3',
+      body: 'Problem With Images.',
+    })
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
+
+async function updateIssue() {
+  const octokit = new Octokit({
+    auth: ACCESS_TOKEN
+  })
+
+  await octokit.request('PATCH /repos/{owner}/{repo}/issues/{issue_number}', {
+    owner: OWNER,
+    repo: REPO,
+    issue_number: '2',
+    title: 'This is Dheeraj',
+  })
+}
+
+async function lockIssue() {
+const octokit = new Octokit({
+  auth: ACCESS_TOKEN
+})
+
+await octokit.request('PUT /repos/{owner}/{repo}/issues/{issue_number}/lock', {
+  owner: OWNER,
+  repo: REPO,
+  issue_number: '1',
+  lock_reason: 'off-topic',
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
+}
+
+async function unlockIssue(){
+const octokit = new Octokit({
+  auth: ACCESS_TOKEN
+})
+
+await octokit.request('DELETE /repos/{owner}/{repo}/issues/{issue_number}/lock', {
+  owner: OWNER,
+    repo: REPO,
+    issue_number: '1',
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
+}
+
+// getIssues();
+// createdIssue();
+// updateIssue();
+// lockIssue();
+// unlockIssue();
